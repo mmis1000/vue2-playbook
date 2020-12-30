54f1c1f1470190f98b5b80806442694c48ceea2b
ci-build <ci-build@mmis1000.me>
2020-12-30 14:08:59 +0000

    [skip ci] Updates

--- js/about.12c70956.js
+++ js/about.12c70956.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an playbook intended for test vue pattern magics.")])])}],i=n("2877"),r={},u=Object(i["a"])(r,a,s,!1,null,null,null);e["default"]=u.exports}}]);
-//# sourceMappingURL=about.5d3908eb.js.map
+//# sourceMappingURL=about.12c70956.js.map
